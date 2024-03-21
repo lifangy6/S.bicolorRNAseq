25bb524296b7f3862f64dc4844f7910384503f5d
Fangyi Li <fangyi.li@mail.utoronto.ca>
2024-03-21 01:25:11 -0400

    Update MA plots related

--- r/run_MAPlots.R
+++ r/run_MAPlots.R
@@ -10,7 +10,7 @@ dds_seed_wt_plasma <- readRDS("E:/S.bicolorRNAseq/data/rds/dds_seed_wt_plasma.rd
 dds_shoot_wt_plasma <- readRDS("E:/S.bicolorRNAseq/data/rds/dds_shoot_wt_plasma.rds")
 
 # MA plots
-DESeq2::plotMA(dds_seed_wt_plasma, alpha = 0.1, main = "MA Plot - seed_wt vs. seed_dry (DESeq2, alpha = 0.1)")
-DESeq2::plotMA(dds_seed_wt_dry, alpha = 0.1, main = "MA Plot - seed_wt vs. seed_plasma (DESeq2, alpha = 0.1)")
-DESeq2::plotMA(dds_shoot_wt_plasma, alpha = 0.1, main = "MA Plot - shoot_wt vs. shoot_plasma (DESeq2, alpha = 0.1)")
+DESeq2::plotMA(dds_seed_wt_plasma, alpha = 0.1, main = "MA Plot - Seed WT vs. Dry (DESeq2, alpha = 0.1)")
+DESeq2::plotMA(dds_seed_wt_dry, alpha = 0.1, main = "MA Plot - Seed WT vs. Plasma (DESeq2, alpha = 0.1)")
+DESeq2::plotMA(dds_shoot_wt_plasma, alpha = 0.1, main = "MA Plot - Shoot WT vs. Plasma (DESeq2, alpha = 0.1)")
 
