72900ac0f05dce83043fbcf8c3d2214e5180b8a9
Fangyi Li <fangyi.li@mail.utoronto.ca>
2024-03-21 01:16:14 -0400

    Update MA plots related (code, rds, figures...)

--- r/run_MAPlots.R
+++ r/run_MAPlots.R
@@ -5,11 +5,12 @@ BiocManager::install("DESeq2")
 library(DESeq2)
 
 # Load DESeq2 results
-res_seed_wt_dry <- readRDS("E:/S.bicolorRNAseq/data/rds/res_seed_wt_dry.rds")
-res_seed_wt_plasma <- readRDS("E:/S.bicolorRNAseq/data/rds/res_seed_wt_plasma.rds")
-res_shoot_wt_plasma <- readRDS("E:/S.bicolorRNAseq/data/rds/res_shoot_wt_plasma.rds")
+dds_seed_wt_dry <- readRDS("E:/S.bicolorRNAseq/data/rds/dds_seed_wt_dry.rds")
+dds_seed_wt_plasma <- readRDS("E:/S.bicolorRNAseq/data/rds/dds_seed_wt_plasma.rds")
+dds_shoot_wt_plasma <- readRDS("E:/S.bicolorRNAseq/data/rds/dds_shoot_wt_plasma.rds")
 
 # MA plots
-DESeq2::plotMA(res_seed_wt_plasma, alpha = 0.1, main = "MA Plot: seed_wt vs. seed_dry (alpha = 0.1)")
-DESeq2::plotMA(res_seed_wt_dry, alpha = 0.1, main = "MA Plot: seed_wt vs. seed_plasma (alpha = 0.1)")
-DESeq2::plotMA(res_shoot_wt_plasma, alpha = 0.1, main = "MA Plot: shoot_wt vs. shoot_plasma (alpha = 0.1)")
+DESeq2::plotMA(dds_seed_wt_plasma, alpha = 0.1, main = "MA Plot - seed_wt vs. seed_dry (DESeq2, alpha = 0.1)")
+DESeq2::plotMA(dds_seed_wt_dry, alpha = 0.1, main = "MA Plot - seed_wt vs. seed_plasma (DESeq2, alpha = 0.1)")
+DESeq2::plotMA(dds_shoot_wt_plasma, alpha = 0.1, main = "MA Plot - shoot_wt vs. shoot_plasma (DESeq2, alpha = 0.1)")
+
